dd66387a887ac6c6f9226933c95b04e2c52ccde5
sysim <liv660n@gmail.com>
2021-10-03 17:16:49 +0900

    게시글 등록 템플릿 추가

--- src/main/java/com/myweb/springboot/web/IndexController.java
+++ src/main/java/com/myweb/springboot/web/IndexController.java
@@ -7,4 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     @GetMapping("/")
     public String index() {return "index";}
+
+    @GetMapping("/posts/save")
+    public String postsSave() {return "posts-save";}
 }
